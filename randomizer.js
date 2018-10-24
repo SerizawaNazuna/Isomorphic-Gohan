87ca6a8da075b971bd51421000683d8a93b70803
SerizawaNazuna <k.capricorn.s@gmail.com>
2018-10-24 13:37:44 +0900

    added codes of line bot

--- randomizer.js
+++
@@ -1,8 +0,0 @@
-exports.randomize = (rest) =>{
-    let randomizedRest = rest
-    while(randomizedRest.length > 10){
-        const index = Math.floor(Math.random() * (randomizedRest.length + 1))
-        randomizedRest.splice(index, 1)
-    }
-    return randomizedRest
-}
